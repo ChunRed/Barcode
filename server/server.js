de7efed359e5d8308958f30d562d8d26a7874790
sonicstride <sonicstride.tw@gmail.com>
2024-06-15 17:49:13 +0800

    init

---
+++ server/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const http = require('http').Server(app);
+const engine = require('ejs-locals');
+const path = require('path');
+const fs = require('fs');
+
+const https = require('https').Server(
+    {
+        key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
+        cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem')),
+    },
+    app
+);
+
+
+
+
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.static('node_modules'));
+app.use(express.static('../client'));
+
+app.set('views', path.join(__dirname, '../client'));
+
+// setup express router
+app.get('/', function (req, res) {
+    res.render('index');
+}); 
+
+let PORT = 3000;
+https.listen(PORT, () => console.log(`Server running on port ${PORT}`));
